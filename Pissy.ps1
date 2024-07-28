81347dc1aa34d1b61833de1a1bac7f71fe26d71f
Remi V <126026464+Revermb@users.noreply.github.com>
2024-07-28 02:15:00 +0000

    Update Pissy.ps1
    added a windows debloater by Raphire https://github.com/Raphire/Win11Debloat

--- Pissy.ps1
+++ Pissy.ps1
@@ -2,23 +2,22 @@
 #Requires -RunAsAdministrator
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 Set-Location D:
-
 #Getting all exe's installed
 $installers = Get-ChildItem ".\Installers" -Filter "*.exe"
-foreach ($inst in $installers) {
+foreach ($inst in $installers)
+{
     Write-Host "----------------------------------------------"
     Write-Host "Installing $inst "
     Start-Process -FilePath $inst.FullName -Wait
 }
-
 #Getting all msi's installed
 $installers = Get-ChildItem ".\Installers" -Filter "*.msi"
-foreach ($inst in $installers) {
+foreach ($inst in $installers)
+{
     Write-Host "----------------------------------------------"
     Write-Host "Installing $inst "
     Start-Process -FilePath $inst.FullName -Wait
 }
-
 #Getting windows updates
 Write-Host "----------------------------------------------"
 Write-Host "Installing NuGet module"
@@ -48,4 +47,16 @@ Write-Host "Unstalling NuGet module"
 Write-Host "----------------------------------------------"
 Write-Host "Unstalling Windows Updater module"
 uninstall-Module -Name PSWindowsUpdate -Force
-#Restart-Computer -Force
+Write-Host "----------------------------------------------"
+
+Write-Host "----------------------------------------------"
+Write-Host "Installing Win11Debloat by Raphire (https://github.com/Raphire/Win11Debloat)"
+try{
+    & ([scriptblock]::Create((Invoke-RestMethod "https://win11debloat.raphi.re/"))) -Wait
+}
+catch{
+    & ([scriptblock]::Create((Invoke-RestMethod "https://win11debloat.raphi.re/")))
+}
+Write-Host "----------------------------------------------"
+Write-Host "Special thanks to all the members of the Drewski Discord for no particular reason whatsoever"
+Restart-Computer -ForceS
